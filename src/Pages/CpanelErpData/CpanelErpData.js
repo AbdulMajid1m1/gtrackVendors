e36ddbb6e74c020db05cf716cabd7e25ab07ffb5
fawadshah95 <fawad.shah@enviovista.com>
2023-10-16 14:11:23 +0500

    Commit

--- src/Pages/CpanelErpData/CpanelErpData.js
+++ src/Pages/CpanelErpData/CpanelErpData.js
@@ -31,6 +31,7 @@ const CpanelErpData = () => {
   const [manufacturing, setManufacturing] = useState([])
   const [salesInvoice, setsalesInvoice] = useState([])
   const [poProduct, setPoProduct] = useState([])
+  const [error, setError] = useState(null);
   const credentials = JSON.parse(localStorage.getItem("credentials"))
 //   console.log(credentials)
   const vendorData = JSON.parse(sessionStorage.getItem("vendorData"))
@@ -239,31 +240,31 @@ const CpanelErpData = () => {
 
 
   // popup code
-//   const [showPopup, setShowPopup] = useState(false);
-//   const [vendorsList, setVendorsList] = useState([]);
-//   const [selectedVendorId, setSelectedVendorId] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
+  const [vendorsList, setVendorsList] = useState([]);
+  const [selectedVendorId, setSelectedVendorId] = useState('');
 
-//   const handleAddUserPopup = async () => {
-//     setShowPopup(true);
-//     try {
-//       const res = await newRequest.get("/getVendorsByMemberId?memberId=" + currentUser?.user?.id);
-//       // filter it for status ""approve""
-//       console.log(res.data);
-//       const vendorsData = res?.data?.filter(item => item?.status === "approve")
-//       setVendorsList(vendorsData);
-//     } catch (error) {
-//       console.log(error);
-//       setError(error?.response?.data?.message || 'Something went wrong');
-//     }
-//   };
+  const handleAddUserPopup = async () => {
+    setShowPopup(true);
+    try {
+      const res = await newRequest.get(`/getSupplierInternalUserByVendorId?vendor_id=${vendorData?.user?.id}`);
+      // filter it for status ""approve""
+      console.log(res.data);
+      // const vendorsData = res?.data?.filter(item => item?.status === "approve")
+      setVendorsList(res?.data || []);
+    } catch (error) {
+      console.log(error);
+      setError(error?.response?.data?.message || 'Something went wrong');
+    }
+  };
 
-//   const handleSendPOClick = () => {
-//     setShowPopup(true);
-//   };
+  const handleSendPOClick = () => {
+    setShowPopup(true);
+  };
 
-//   const handleAddUserClose = () => {
-//     setShowPopup(false);
-//   };
+  const handleAddUserClose = () => {
+    setShowPopup(false);
+  };
 
   
 //   const handleGeneratePdf = () => {
@@ -416,6 +417,17 @@ const CpanelErpData = () => {
 
 
 
+        {/* Popup Button Assign PickList */}
+        <div className="flex justify-end gap-3 mt-6">
+          <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" 
+          onClick={handleAddUserPopup}
+          >
+            Assign PickList
+          </button>
+        </div>
+
+
+
         <div>
           <div style={{ marginLeft: '-11px', marginRight: '-11px', }}>
             {activeTab === 'Purchase-Order' && (
@@ -426,7 +438,7 @@ const CpanelErpData = () => {
                 <div className='w-full md:w-[50%]'>
                   <DataTable
                     data={purshase}
-                    title={'Purchase Order'}
+                    title={'Sales order'}
                     columnsName={purchaseOrderColumns}
                     processRowUpdate={processRowUpdate}
                     // loading={isLoading}
@@ -455,7 +467,7 @@ const CpanelErpData = () => {
                     columnsName={orderLineColumns}
                     loading={poProductLoading}
                     processRowUpdate={processRowUpdate}
-                    checkboxSelection="disabled"
+                    // checkboxSelection="disabled"
                     secondaryColor="secondary"
                     actionColumnVisibility={false}
                     uniqueId={"purchaseOrderProductId"}
@@ -498,13 +510,13 @@ const CpanelErpData = () => {
 
 
         {/* popup screen */}
-        {/* {showPopup && (
+        {showPopup && (
           <div className="popup-container fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-40">
             <div className="popup bg-white rounded-lg shadow-xl overflow-hidden max-w-md m-4">
               <div className="header bg-primary text-white font-bold py-4 px-6">
                 <h2 style={{ color: "white" }}>SEND PO</h2>
               </div>
-              <form onSubmit={handlePOFormSubmit} className="p-6">
+              {/* <form onSubmit={handlePOFormSubmit} className="p-6"> */}
                 <label htmlFor="UserName" className="block mb-2 text-gray-700 text-sm">Name:</label>
                 <select
                   id="UserName"
@@ -516,7 +528,7 @@ const CpanelErpData = () => {
                   <option value="">--Select Vendor--</option>
                   {vendorsList?.map((user) => (
                     <option key={user?.id} value={user?.id}>
-                      {user?.company_name_English} - {user?.email}
+                      {user?.vendor_id} - {user?.user_email}
                     </option>
                   ))}
                 </select>
@@ -525,10 +537,10 @@ const CpanelErpData = () => {
                   <button className="close-btn text-white bg-secondary hover:bg-red-600 rounded-lg px-6 py-2" type="button" onClick={handleAddUserClose}>CANCEL</button>
                   <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" type="submit">SEND</button>
                 </div>
-              </form>
+              {/* </form> */}
             </div>
           </div>
-        )} */}
+        )}
 
       </div>
     </div >
