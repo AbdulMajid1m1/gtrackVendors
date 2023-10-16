fdb2386aa3f49c1b4f01b0a79ae0687b76bf7f05
unknown <abdulmajid1m2@gmail.com>
2023-10-16 22:08:29 +0500

    added email fun

--- src/Pages/CpanelErpData/CpanelErpData.js
+++ src/Pages/CpanelErpData/CpanelErpData.js
@@ -11,8 +11,6 @@ import { DataTableContext } from '../../Contexts/DataTableContext';
 
 const CpanelErpData = () => {
   const [activeTab, setActiveTab] = useState('Purchase-Order');
-//   const { currentUser } = useContext(CurrentUserContext);
-//   console.log(currentUser)
   const [isLoading, setIsLoading] = useState(true);
   const [poProductLoading, setPoProductLoading] = useState(true);
 
@@ -26,13 +24,13 @@ const CpanelErpData = () => {
   const [salesInvoice, setsalesInvoice] = useState([])
   const [poProduct, setPoProduct] = useState([])
   const [error, setError] = useState(null);
-  
+
   const credentials = JSON.parse(localStorage.getItem("credentials"))
-//   console.log(credentials)
   const vendorData = JSON.parse(sessionStorage.getItem("vendorData"))
-//   console.log(vendorData?.user)
+  // console.log(vendorData?.user)
+
   const [poName, setPoName] = useState('')
-  
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -42,7 +40,6 @@ const CpanelErpData = () => {
         const response = await newRequest.get(`/getPOHeaderBySupplierId?supplier_id=${vendorData.user.id}`);
         console.log(response.data);
         setIsLoading(false)
-
         setPurshase(response?.data || []);
         const poHeaderId = response?.data[0]?.po_header_id; // Extract the po_header_id
 
@@ -110,44 +107,9 @@ const CpanelErpData = () => {
   };
 
 
-//   const [salesOrderFetched, setsalesOrderFetched] = useState(false);
-//   const handleSalesOrder = async () => {
-//     setIsLoading(true);
-//     try {
-//       // /member/gln/list
-//       const response = await newRequest.post("/getSalesOrders", {
-//         // id: currentUser?.user?.id
-//         id: credentials?.id,
-//       });
-//       console.log(response.data);
-//       setsalesInvoice(response?.data || []);
-//       setIsLoading(false)
-//       setsalesOrderFetched(true);
-//     }
-//     catch (err) {
-//       setIsLoading(false)
-//       Swal.fire({
-//         icon: 'error',
-//         title: 'Oops...',
-//         text: err?.response?.data?.message || 'Something went wrong!',
-//       })
 
-
-//       console.log(err);
-//       setIsLoading(false)
-//     }
-//   };
-//   const handleSalesOrderTabClick = () => {
-//     if (!salesOrderFetched) {
-//       handleSalesOrder();
-//     }
-//     setActiveTab('Sales-Order');
-//   };
-
-  
   const handleRowClickInParent = async (row) => {
     console.log(row)
-    setPoName(row[0]?.po_header_id)
     try {
       // if  click on same row again then dont call api again
       if (row[0]?.po_header_id === poName) {
@@ -155,7 +117,7 @@ const CpanelErpData = () => {
       }
       setPoProductLoading(true)
       const ProductResponse = await newRequest.get(`/getPODetailsByPoHeaderId?po_header_id=${row[0]?.po_header_id}`);
-
+      setPoName(row[0]?.po_header_id)
       console.log(ProductResponse.data);
       setPoProduct(ProductResponse?.data || []);
       setIsLoading(false)
@@ -192,21 +154,6 @@ const CpanelErpData = () => {
       case ActiveTabs.MANUFACTURING:
         return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updateProductionData", credentials?.id);
 
-    //   case ActiveTabs.SALES_ORDER:
-    //     return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updateSalesOrderData", credentials?.id);
-
-    //   case ActiveTabs.SALES_INVOICE:
-    //     return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updateSalesInvoicesData", credentials?.id);
-
-    //   case ActiveTabs.INVENTORY_STOCK_MASTER:
-    //     return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updateInventoryData", credentials?.id);
-
-    //   case ActiveTabs.VENDORS_SUPPLIERS_DATA:
-    //     return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updateVendorsSuppliersData", credentials?.id);
-
-    //   case ActiveTabs.FIXED_ASSETS_DATA:
-    //     return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updateFixedAssetsData", credentials?.id);
-
       default:
         console.log("default");
         return;
@@ -218,148 +165,36 @@ const CpanelErpData = () => {
 
 
   // popup code
-//   const [showPopup, setShowPopup] = useState(false);
-//   const [vendorsList, setVendorsList] = useState([]);
-//   const [selectedVendorId, setSelectedVendorId] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
+  const [vendorsList, setVendorsList] = useState([]);
+  const [selectedVendorId, setSelectedVendorId] = useState('');
 
-//   const handleAddUserPopup = async () => {
-//     setShowPopup(true);
-//     try {
-//       const res = await newRequest.get("/getVendorsByMemberId?memberId=" + currentUser?.user?.id);
-//       // filter it for status ""approve""
-//       console.log(res.data);
-//       const vendorsData = res?.data?.filter(item => item?.status === "approve")
-//       setVendorsList(vendorsData);
-//     } catch (error) {
-//       console.log(error);
-//       setError(error?.response?.data?.message || 'Something went wrong');
-//     }
-//   };
+  const handleAddUserPopup = async () => {
+    // add the condition to select the row on the tableSelectedRows 
+    if (tableSelectedRows.length === 0) {
+      openSnackbar('Please select atleast one row', 'error');
+      return;
+    }
+    setShowPopup(true);
+    try {
+      const res = await newRequest.get(`/getSupplierInternalUserByVendorId?vendor_id=${vendorData?.user?.id}`);
 
-//   const handleSendPOClick = () => {
-//     setShowPopup(true);
-//   };
+      console.log(res.data);
+      // const vendorsData = res?.data?.filter(item => item?.status === "approve")
+      setVendorsList(res?.data || []);
+    } catch (error) {
+      console.log(error);
+      setError(error?.response?.data?.message || 'Something went wrong');
+    }
+  };
 
-//   const handleAddUserClose = () => {
-//     setShowPopup(false);
-//   };
+  const handleSendPOClick = () => {
+    setShowPopup(true);
+  };
 
-  
-//   const handleGeneratePdf = () => {
-    
-//   }
-   
-
-//   const handlePOFormSubmit = async (e) => {
-//     e.preventDefault();
-
-//     const vendor = vendorsList.find(v => v.id === parseInt(selectedVendorId));
-
-//     const childQuantities = poProduct?.map(line => ({
-//       epcClass: line.name,
-//       quantity: line.quantity
-//     }));
-
-
-//     const mappedData = {
-//       EventType: "TransactionEvent",
-//       EventAction: "OBSERVE",
-//       EventTime: new Date().toISOString(),
-//       EventTimeZoneOffSet: "2023-10-14T00:00:00Z",
-//       epcList: `${vendor.company_name_English}-${vendor.email}`,
-//       bizStep: "ordering",
-//       disposition: "in_transit",
-//       bizTransactionList: JSON.stringify([{ type: "po", bizTransaction: vendor.company_name_English }]),
-//       readPoint: `{"id":"${vendor.website}"}`,
-//       sourceList: `{"type":"location","source":"${currentUser?.user?.company_name_English}"}`,
-//       destinationList: `{"type":"location","destination":"${vendor.company_name_English}"}`,
-//       sensorElementList: "",
-//       childQuantityList: JSON.stringify(childQuantities),
-//       childEPCs: "",
-//       parentID: "",
-//       inputEPCList: "",
-//       inputQuantityList: "",
-//       outputEPCList: "",
-//       ilmd: "",
-//       eventID: `${vendor.id}`,
-//       errorDeclaration: "",
-//       quantityList: "1", // Assuming 1 order per purchase, adjust if necessary
-//       persistentDisposition: "ordering",
-//       creationDate: new Date().toISOString(),
-//       sender: currentUser?.user?.company_name_English,
-//       receiver: vendor.company_name_English,
-//       instanceIdentifer: `${vendor.id}`
-//     };
-
-//     console.log(mappedData);
-
-//     try {
-//       // First, insert the PO header
-//       const poHeaderResponse = await newRequest.post("/insertPOHeader", {
-//         member_id: currentUser?.user?.id,
-//         create_date: new Date(),
-//         supplier_id: vendor.id,
-//       });
-//       console.log(poHeaderResponse);
-
-//       if (!poHeaderResponse.data?.insertId) {
-//         Swal.fire({
-//           title: 'Error!',
-//           text: 'Failed to insert PO Header',
-//           icon: 'error',
-//           confirmButtonText: 'Close'
-//         });
-//       }
-
-//       // Use the returned insertId in insertMultiplePODetails API
-//       console.log(poProduct);
-//       const poDetailsResponse = await newRequest.post("/insertMultiplePODetails", poProduct.map(line => ({
-//         po_header_id: poHeaderResponse.data.insertId,
-//         product_name: line.name,
-//         quantity: line.quantity,
-//         price: line.price,
-//         price_subtotal: line.price_subtotal,
-//         price_total: line.price_total,
-//         date_order: line.date_order,
-//         state: line.state,
-//         partner_name: line.partner
-//       })));
-
-//       console.log(poDetailsResponse);
-
-//       if (!poDetailsResponse.data?.insertIds || poDetailsResponse.data.insertIds.length !== poProduct.length) {
-//         Swal.fire({
-//           title: 'Error!',
-//           text: 'Failed to insert PO Details',
-//           icon: 'error',
-//           confirmButtonText: 'Close'
-//         });
-//       }
-
-
-
-//       // Finally, insert the EPCIS event
-//       const epcisResponse = await newRequest.post("/insertEPCISEvent", mappedData);
-//       console.log(epcisResponse);
-//       Swal.fire({
-//         title: 'Success!',
-//         text: 'All data inserted successfully.',
-//         icon: 'success',
-//         confirmButtonText: 'OK'
-//       });
-//       setShowPopup(false);
-//     } catch (err) {
-//       console.log(err);
-//       Swal.fire({
-//         title: 'Error!',
-//         text: err?.response?.data?.message || 'Something went wrong',
-//         icon: 'error',
-//         confirmButtonText: 'Close'
-//       });
-//     };
-//   };
-
-
+  const handleAddUserClose = () => {
+    setShowPopup(false);
+  };
 
 
   return (
@@ -397,8 +232,8 @@ const CpanelErpData = () => {
 
         {/* Popup Button Assign PickList */}
         <div className="flex justify-end gap-3 mt-6">
-          <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" 
-          onClick={handleAddUserPopup}
+          <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2"
+            onClick={handleAddUserPopup}
           >
             Assign PickList
           </button>
@@ -441,9 +276,7 @@ const CpanelErpData = () => {
 
                 <div className='w-full md:w-[50%]'>
                   <DataTable data={poProduct}
-                  
-                    title={'Purchase Order Products' + (poName ? ` (PO# ${poName})` : '')}
-
+                    title={'Purchase Order Products'}
                     columnsName={orderLineColumns}
                     loading={poProductLoading}
                     processRowUpdate={processRowUpdate}
@@ -497,26 +330,26 @@ const CpanelErpData = () => {
                 <h2 style={{ color: "white" }}>SEND PO</h2>
               </div>
               {/* <form onSubmit={handlePOFormSubmit} className="p-6"> */}
-                <label htmlFor="UserName" className="block mb-2 text-gray-700 text-sm">Name:</label>
-                <select
-                  id="UserName"
-                  value={selectedVendorId}
-                  onChange={(e) => setSelectedVendorId(e.target.value)}
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-primary"
-                >
-                  <option value="">--Select Vendor--</option>
-                  {vendorsList?.map((user) => (
-                    <option key={user?.id} value={user?.id}>
-                      {user?.vendor_id} - {user?.user_email}
-                    </option>
-                  ))}
-                </select>
+              <label htmlFor="UserName" className="block mb-2 text-gray-700 text-sm">Name:</label>
+              <select
+                id="UserName"
+                value={selectedVendorId}
+                onChange={(e) => setSelectedVendorId(e.target.value)}
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-primary"
+              >
+                <option value="">--Select Vendor--</option>
+                {vendorsList?.map((user) => (
+                  <option key={user?.id} value={user?.id}>
+                    {user?.vendor_id} - {user?.user_email}
+                  </option>
+                ))}
+              </select>
 
-                <div className="flex justify-end gap-3 mt-6">
-                  <button className="close-btn text-white bg-secondary hover:bg-red-600 rounded-lg px-6 py-2" type="button" onClick={handleAddUserClose}>CANCEL</button>
-                  <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" type="submit">SEND</button>
-                </div>
+              <div className="flex justify-end gap-3 mt-6">
+                <button className="close-btn text-white bg-secondary hover:bg-red-600 rounded-lg px-6 py-2" type="button" onClick={handleAddUserClose}>CANCEL</button>
+                <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" type="submit">SEND</button>
+              </div>
               {/* </form> */}
             </div>
           </div>
