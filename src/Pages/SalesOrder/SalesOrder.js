88dabfa0becf3696b389914af97b52d56b355493
fawadshah95 <fawad.shah@enviovista.com>
2023-10-17 12:11:55 +0500

    Commit

--- src/Pages/SalesOrder/SalesOrder.js
+++ src/Pages/SalesOrder/SalesOrder.js
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
 import SendIcon from '@mui/icons-material/Send';
 import { SnackbarContext } from '../../Contexts/SnackbarContext';
 import { DataTableContext } from '../../Contexts/DataTableContext';
-import { UpdateOdooErpRowData } from '../../utils/Funtions/rowUpdate';
+
 
 const SalesOrder = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,12 +20,12 @@ const SalesOrder = () => {
   const [purshase, setPurshase] = useState([])
   const [poProduct, setPoProduct] = useState([])
   const [error, setError] = useState(null);
-
+  
   const credentials = JSON.parse(localStorage.getItem("credentials"))
   const vendorData = JSON.parse(sessionStorage.getItem("vendorData"))
-  // console.log(vendorData?.user)
-
-  const [poName, setPoName] = useState('')
+    // console.log(vendorData?.user)
+  
+    const [poName, setPoName] = useState('')
 
 
   useEffect(() => {
@@ -68,7 +68,7 @@ const SalesOrder = () => {
   }, []); // Empty array dependency ensures this useEffect runs once on component mount
 
 
-
+  
   const handleRowClickInParent = async (row) => {
     console.log(row)
     try {
@@ -97,15 +97,22 @@ const SalesOrder = () => {
     }
   }
 
-  const ActiveTabs = Object.freeze({
-    PURCHASE_ORDER: 'Purchase-Order',
-
-  });
-
 
   const processRowUpdate = (newRow, oldRow) => {
-    UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updatePurchaseOrderData", credentials?.id);
-    return;
+    console.log(newRow, oldRow);
+    switch (activeTab) {
+      case ActiveTabs.PURCHASE_ORDER:
+        return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updatePurchaseOrderData", credentials?.id);
+
+      case ActiveTabs.MANUFACTURING:
+        return UpdateOdooErpRowData(newRow, oldRow, openSnackbar, "/updateProductionData", credentials?.id);
+
+      default:
+        console.log("default");
+        return;
+    }
+
+
   };
 
 
@@ -122,24 +129,24 @@ const SalesOrder = () => {
 
   const handleAddUserPopup = async () => {
     if (tableSelectedRows.length === 0) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Please select atleast one row',
-        timer: 2000,
-        timerProgressBar: true,
-
-      })
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please select atleast one row',
+            timer: 2000,
+            timerProgressBar: true,
+            
+          })
       return;
     }
-
+  
     setShowPopup(true);
     try {
       const res = await newRequest.get(`/getSupplierInternalUserByVendorId?vendor_id=${vendorData?.user?.id}`);
-
+     
       console.log(res.data);
       setVendorsList(res?.data || []);
-
+    
     } catch (error) {
       console.log(error);
       setError(error?.response?.data?.message || 'Something went wrong');
@@ -160,13 +167,13 @@ const SalesOrder = () => {
   const handleSalesPickingList = async (e) => {
     e.preventDefault();
     try {
-      // Create an array with the body data for multiple rows
+       // Create an array with the body data for multiple rows
       const requestBody = tableSelectedRows.map((selectedRow) => ({
         po_detail_id: selectedRow.po_detail_id,
         po_header_id: selectedRow.po_header_id,
         assign_to_user_id: selectedVendorId?.user_id,
       }));
-
+     
       const res = await newRequest.post(`/insertSalesPickingList`, requestBody);
       console.log(res.data);
       Swal.fire({
@@ -183,7 +190,7 @@ const SalesOrder = () => {
       setError(error?.response?.data?.message || 'Something went wrong');
     }
   }
-
+  
 
   return (
     <div>
@@ -191,8 +198,8 @@ const SalesOrder = () => {
 
         {/* Popup Button Assign PickList */}
         <div className="flex justify-end gap-3 -mt-4">
-          <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2"
-            onClick={handleAddUserPopup}
+          <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" 
+          onClick={handleAddUserPopup}
           >
             Assign PickList
           </button>
@@ -200,53 +207,53 @@ const SalesOrder = () => {
 
 
         <div>
-          <div style={{ marginLeft: '-11px', marginRight: '-11px', marginTop: '-10px' }}>
+          <div style={{ marginLeft: '-11px', marginRight: '-11px', marginTop: '-10px'}}>
             {/* {activeTab === 'Purchase-Order' && ( */}
 
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+              <div style={{ display: 'flex', flexWrap: 'wrap' }}>
 
-              <div className='w-full md:w-[50%]'>
-                <DataTable
-                  data={purshase}
-                  title={'Sales order'}
-                  columnsName={purchaseOrderColumns}
-                  processRowUpdate={processRowUpdate}
-                  // loading={isLoading}
-                  secondaryColor="secondary"
-                  checkboxSelection="disabled"
-                  uniqueId={"purchaseOrderId"}
-                  handleRowClickInParent={handleRowClickInParent}
-                  actionColumnVisibility={false}
-                  dropDownOptions={[
-                    {
-                      label: "Send PO",
-                      icon: <SendIcon fontSize="small" color="action" style={{ color: "rgb(37 99 235)" }} />
-                      ,
-                      // action: handleAddUserPopup, // Open the popup when this button is clicked
+                <div className='w-full md:w-[50%]'>
+                  <DataTable
+                    data={purshase}
+                    title={'Sales order'}
+                    columnsName={purchaseOrderColumns}
+                    processRowUpdate={processRowUpdate}
+                    // loading={isLoading}
+                    secondaryColor="secondary"
+                    checkboxSelection="disabled"
+                    uniqueId={"purchaseOrderId"}
+                    handleRowClickInParent={handleRowClickInParent}
+                    actionColumnVisibility={false}
+                    dropDownOptions={[
+                      {
+                        label: "Send PO",
+                        icon: <SendIcon fontSize="small" color="action" style={{ color: "rgb(37 99 235)" }} />
+                        ,
+                        // action: handleAddUserPopup, // Open the popup when this button is clicked
 
-                    },
+                      },
 
-                  ]}
+                    ]}
 
-                />
+                  />
+                </div>
+
+                <div className='w-full md:w-[50%]'>
+                  <DataTable data={poProduct}
+                    title={'Purchase Order Products'}
+                    columnsName={orderLineColumns}
+                    loading={poProductLoading}
+                    processRowUpdate={processRowUpdate}
+                    // checkboxSelection="disabled"
+                    secondaryColor="secondary"
+                    actionColumnVisibility={false}
+                    uniqueId={"purchaseOrderProductId"}
+
+
+                  />
+                </div>
+
               </div>
-
-              <div className='w-full md:w-[50%]'>
-                <DataTable data={poProduct}
-                  title={'Purchase Order Products'}
-                  columnsName={orderLineColumns}
-                  loading={poProductLoading}
-                  processRowUpdate={processRowUpdate}
-                  // checkboxSelection="disabled"
-                  secondaryColor="secondary"
-                  actionColumnVisibility={false}
-                  uniqueId={"purchaseOrderProductId"}
-
-
-                />
-              </div>
-
-            </div>
             {/* )} */}
 
           </div>
@@ -260,28 +267,46 @@ const SalesOrder = () => {
               <div className="header bg-primary text-white font-semibold py-4 px-6">
                 <h2 style={{ color: "white" }}>Assigned PickList To User</h2>
               </div>
-              {/* <form onSubmit={handlePOFormSubmit} className="p-6"> */}
-              <label htmlFor="UserName" className="block mb-2 text-gray-700 text-sm">Name:</label>
-              <select
-                id="UserName"
-                value={selectedVendorId}
-                onChange={(e) => setSelectedVendorId(e.target.value)}
-                required
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-primary"
-              >
-                <option value="">--Select Vendor--</option>
-                {vendorsList?.map((user) => (
-                  <option key={user?.id} value={user?.id}>
-                    {user?.vendor_id} - {user?.user_email}
-                  </option>
-                ))}
-              </select>
+              <form onSubmit={handleSalesPickingList} className="p-6">
+                <label htmlFor="UserName" className="block mb-2 text-gray-700 text-sm">Name:</label>
+                {/* <select
+                  id="UserName"
+                  value={selectedVendorId}
+                  onChange={(e) => setSelectedVendorId(e.target.value)}
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-primary"
+                >
+                  <option value="">--Select Vendor--</option>
+                  {vendorsList?.map((user) => (
+                    <option key={user?.id} value={user?.id}>
+                      {user?.user_id} - {user?.user_email}
+                    </option>
+                  ))}
+                </select> */}
+                <select
+                  id="UserName"
+                  value={selectedVendorId.user_id} // Use user_id as the value
+                  onChange={(e) => setSelectedVendorId({
+                    user_id: e.target.value,
+                    user_email: e.target.options[e.target.selectedIndex].getAttribute('data-email')
+                  })}
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-primary"
+                >
+                  <option value="">--Select Vendor--</option>
+                  {vendorsList?.map((user) => (
+                    <option key={user?.id} value={user?.user_id} data-email={user?.user_email}>
+                      {user?.vendor_id} - {user?.user_email}
+                    </option>
+                  ))}
+                </select>
 
-              <div className="flex justify-end gap-3 mt-6">
-                <button className="close-btn text-white bg-secondary hover:bg-red-600 rounded-lg px-6 py-2" type="button" onClick={handleAddUserClose}>CANCEL</button>
-                <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" type="submit">SEND</button>
-              </div>
-              {/* </form> */}
+
+                <div className="flex justify-end gap-3 mt-6">
+                  <button className="close-btn text-white bg-secondary hover:bg-red-600 rounded-lg px-6 py-2" type="button" onClick={handleAddUserClose}>CANCEL</button>
+                  <button className="text-white bg-primary hover:bg-blue-600 rounded-lg px-6 py-2" type="submit">SEND</button>
+                </div>
+              </form>
             </div>
           </div>
         )}
